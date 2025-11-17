608610963b782d150c8d69caa3d64cf8992c71b3
kajimurakeiya <kkajimura0153@gmail.com>
2025-11-18 00:28:30 +0900

    cargo build

---
+++ hello_cargo/target/debug/deps/hello_cargo-81050b3912c48ea9.d
@@ -0,0 +1,5 @@
+/Users/kajimurakeiya/rust-play-ground/hello_cargo/target/debug/deps/hello_cargo-81050b3912c48ea9.d: src/main.rs
+
+/Users/kajimurakeiya/rust-play-ground/hello_cargo/target/debug/deps/hello_cargo-81050b3912c48ea9: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/kajimurakeiya/rust-play-ground/hello_cargo/target/debug/hello_cargo: /Users/kajimurakeiya/rust-play-ground/hello_cargo/src/main.rs
